df1b5f3e787c7b43ccc47740316b91ea38728d72
Anshul Dahiya <anshul.rts@gmail.com>
2018-08-22 19:07:41 +0530

    Property Binding Canonical Form (bind-PropName)

--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 export class AppComponent  {
     pageHeader: string = 'Employee Details';
     imgPath: string = 'http://www.pragimtech.com/Images/Logo.JPG';
+    isDisabled: boolean = true;
+
 
     firstName: string = 'Anshul';
     lastName: string = 'Dahiya';
