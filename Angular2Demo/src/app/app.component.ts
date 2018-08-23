9724f9ea200e3a657d65d30aee3f84bb5f6d1cf7
Anshul Dahiya <anshul.rts@gmail.com>
2018-08-23 14:29:09 +0530

    Style and Event binding Here we have binded the click event using on-click We could also have used (click)

--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -9,11 +9,28 @@ export class AppComponent  {
     imgPath: string = 'http://www.pragimtech.com/Images/Logo.JPG';
     isDisabled: boolean = true;
 
-
     firstName: string = 'Anshul';
     lastName: string = 'Dahiya';
 
+    isBold: boolean = true;
+    fontSize: number = 30;
+    isItalic: boolean = true;
+
     getFullName(): string {
         return this.firstName + ' ' + this.lastName;
+    }
+
+    buttonClicked(): void{
+        alert('Button Clicked');
+    }
+
+    addStyles() {
+        let styles = {
+            'font-size.px': this.fontSize,
+            'font-weight': this.isBold ? 'bold' : 'normal',
+            'font-style': this.isItalic ? 'italic' : 'normal'
+        };
+
+        return styles;
     }
 }
