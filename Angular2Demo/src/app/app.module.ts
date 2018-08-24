ffaaa66a9e56c8d8590d23781d6c1eb350c45088
Anshul Dahiya <anshul.rts@gmail.com>
2018-08-24 17:13:48 +0530

    Custom Pipe

--- Angular2Demo/src/app/app.module.ts
+++ Angular2Demo/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { SecondEmployeeComponent } from './secondEmployee/secondEmployee.component';
 import { EmployeeListComponent } from './employee/employeelist.component';
+import { EmployeeTitlePipe } from './employee/employeeTitle.pipe';
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, EmployeeComponent, SecondEmployeeComponent, EmployeeListComponent],
+    declarations: [AppComponent, EmployeeComponent, SecondEmployeeComponent, EmployeeListComponent, EmployeeTitlePipe],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
