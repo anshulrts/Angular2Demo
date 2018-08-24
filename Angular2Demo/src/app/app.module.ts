6f79b93824df2728ae9e1790eef61d374312db12
Anshul Dahiya <anshul.rts@gmail.com>
2018-08-24 12:21:37 +0530

    Event Binding - Better Example (second employee and ngIf)

--- Angular2Demo/src/app/app.module.ts
+++ Angular2Demo/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { SecondEmployeeComponent } from './secondEmployee/secondEmployee.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, EmployeeComponent],
+    declarations: [AppComponent, EmployeeComponent, SecondEmployeeComponent],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ Angular2Demo/src/app/secondEmployee/secondEmployee.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+    selector: 'my-secondEmployee',
+    templateUrl: 'app/secondEmployee/secondEmployee.component.html',
+    styleUrls: ['app/secondEmployee/secondEmployee.component.css']
+})
+
+export class SecondEmployeeComponent {
+    firstName: string = 'John';
+    lastName: string = 'Hopkins';
+    gender: string = 'Male';
+    age: number = 23;
+    columnSpan: number = 2;
+    showDetails = false;
+
+    toggleDetails(): void {
+        this.showDetails = !this.showDetails;
+    }
+}
