82164675fe6d66b4c79f31b911c8d8c3625e352f
Anshul Dahiya <anshul.rts@gmail.com>
2018-08-24 16:18:48 +0530

    Using Angular Pipes

--- Angular2Demo/src/app/employee/employeelist.component.ts
+++ Angular2Demo/src/app/employee/employeelist.component.ts
@@ -8,10 +8,10 @@
 
 export class EmployeeListComponent {
     employees: any[] = [
-        { code: "E100", name: "Anshul", gender: "Male", salary: "2000.45", dateofbirth: "01/30/1993" },
-        { code: "E101", name: "Vikram", gender: "Male", salary: "2000", dateofbirth: "07/14/2000" },
-        { code: "E102", name: "Shushil", gender: "Male", salary: "2000", dateofbirth: "09/12/1994" },
-        { code: "E103", name: "Pooja", gender: "Female", salary: "2000", dateofbirth: "12/13/2009" },
-        { code: "E104", name: "Kunal", gender: "Male", salary: "2000.2", dateofbirth: "08/17/1998" }
+        { code: "Emp100", name: "Anshul", gender: "Male", salary: "2000.45", dateofbirth: "01/30/1993" },
+        { code: "Emp101", name: "Vikram", gender: "Male", salary: "2000", dateofbirth: "07/14/2000" },
+        { code: "Emp102", name: "Shushil", gender: "Male", salary: "2000", dateofbirth: "09/12/1994" },
+        { code: "Emp103", name: "Pooja", gender: "Female", salary: "2000", dateofbirth: "12/13/2009" },
+        { code: "Emp104", name: "Kunal", gender: "Male", salary: "2000.2", dateofbirth: "08/17/1998" }
     ];
 }
