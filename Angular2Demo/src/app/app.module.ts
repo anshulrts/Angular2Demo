38d5ac0309ca62235900b7051911b316a401a9fb
Anshul Dahiya <anshul.rts@gmail.com>
2018-08-24 14:58:01 +0530

    ngFor

--- Angular2Demo/src/app/app.module.ts
+++ Angular2Demo/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { SecondEmployeeComponent } from './secondEmployee/secondEmployee.component';
+import { EmployeeListComponent } from './employee/employeelist.component';
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, EmployeeComponent, SecondEmployeeComponent],
+    declarations: [AppComponent, EmployeeComponent, SecondEmployeeComponent, EmployeeListComponent],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ Angular2Demo/src/app/employee/employeelist.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+    selector: 'list-employee',
+    templateUrl: '/app/employee/employeelist.component.html',
+    styleUrls: ['/app/employee/employeelist.component.css']
+})
+
+export class EmployeeListComponent {
+    employees: any[] = [
+        { code: "E100", name: "Anshul", gender: "Male", salary: 2000, dateofbirth: "Jan 30, 1993" },
+        { code: "E101", name: "Vikram", gender: "Male", salary: 2000, dateofbirth: "Jan 30, 1993" },
+        { code: "E102", name: "Shushil", gender: "Male", salary: 2000, dateofbirth: "Jan 30, 1993" },
+        { code: "E103", name: "Pooja", gender: "Female", salary: 2000, dateofbirth: "Jan 30, 1993" },
+        { code: "E104", name: "Kunal", gender: "Male", salary: 2000, dateofbirth: "Jan 30, 1993" }
+    ];
+}
