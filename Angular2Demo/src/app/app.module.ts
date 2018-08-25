741e6b30dc3829fdb55f337e888223a312431fc0
Anshul Dahiya <anshul.rts@gmail.com>
2018-08-25 13:34:54 +0530

    Nesting a child component to its parent - To nest a component, use the selector of the component that you want to nest as a directive to the container component

--- Angular2Demo/src/app/app.module.ts
+++ Angular2Demo/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { EmployeeComponent } from './employee/employee.component';
 import { SecondEmployeeComponent } from './secondEmployee/secondEmployee.component';
 import { EmployeeListComponent } from './employee/employeelist.component';
 import { EmployeeTitlePipe } from './employee/employeeTitle.pipe';
+import { EmployeeCountComponent } from './employee/employeeCount.component'
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, EmployeeComponent, SecondEmployeeComponent, EmployeeListComponent, EmployeeTitlePipe],
+    declarations: [AppComponent, EmployeeComponent, SecondEmployeeComponent, EmployeeListComponent,
+        EmployeeTitlePipe, EmployeeCountComponent],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
