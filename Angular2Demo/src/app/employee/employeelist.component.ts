7bdb4fa3c534cd64db433aa2bb0a43287feebd5c
Anshul Dahiya <anshul.rts@gmail.com>
2018-08-25 13:56:52 +0530

    Communication between Container and Nested Component - We can use @Input Properties in child component to define that its value will be passed from Container - Then we can create methods and pass values using Property binding techniques

--- Angular2Demo/src/app/employee/employeelist.component.ts
+++ Angular2Demo/src/app/employee/employeelist.component.ts
@@ -10,8 +10,20 @@ export class EmployeeListComponent {
     employees: any[] = [
         { code: "Emp100", name: "Anshul", gender: "Male", salary: "2000.45", dateofbirth: "01/30/1993" },
         { code: "Emp101", name: "Vikram", gender: "Male", salary: "2000", dateofbirth: "07/14/2000" },
-        { code: "Emp102", name: "Shushil", gender: "Male", salary: "2000", dateofbirth: "09/12/1994" },
+        { code: "Emp102", name: "Shushila", gender: "Female", salary: "2000", dateofbirth: "09/12/1994" },
         { code: "Emp103", name: "Pooja", gender: "Female", salary: "2000", dateofbirth: "12/13/2009" },
         { code: "Emp104", name: "Kunal", gender: "Male", salary: "2000.2", dateofbirth: "08/17/1998" }
     ];
+
+    getAllEmployeeCount(): number {
+        return this.employees.length;
+    }
+
+    getAllMaleEmployeeCount(): number {
+        return this.employees.filter(row => row.gender === "Male").length;
+    }
+
+    getAllFemaleEmployeeCount(): number {
+        return this.employees.filter(row => row.gender === "Female").length;
+    }
 }
